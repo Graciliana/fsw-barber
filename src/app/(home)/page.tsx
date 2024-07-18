f5f0d5e429660a4ad10d393e38a3bc19946ff4fc
Graciliana Kascher <eagraciliana@gmail.com>
2024-07-18 18:03:40 -0300

    [feat] add header of home first

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,25 @@
+import { Separator } from "@radix-ui/react-separator";
 import Header from "../_components/header";
+import { format } from "date-fns";
+import {ptBR} from "date-fns/locale"
 export default function Home() {
   return (
  <div className="bg-background-black">
       <Header />
+     <div className="divide-y divide-blue-200 pb-2"></div>
+      <span className="text-primary-purple text-xl text-bold ml-6">Olá, </span><span className="text-white text-sm"> Faça seu Login!</span><p></p>
+      <span className="text-white text-sm capitalize ml-6">{format(new Date(), "EEEE", {
+        locale: ptBR,
+      })},</span>
+       <span className="text-white text-sm lowercase ml-2">{format(new Date(), "dd", {
+        locale: ptBR,
+       })} de</span>
+        <span className="text-white text-sm capitalize ml-2">{format(new Date(), "MMMM", {
+        locale: ptBR,
+        })}</span>
+       <span className="text-white text-sm capitalize ml-2">{format(new Date(), "yyyy", {
+      }) }</span>
+
  </div>
  
  
