83867066ee29f21e7577d8c53afdc5888f674e35
Graciliana Kascher <eagraciliana@gmail.com>
2024-07-22 18:27:05 -0300

    [feat] add initial barbershop-items star

--- src/app/(home)/_components/barbershop-item.tsx
+++ src/app/(home)/_components/barbershop-item.tsx
@@ -1,6 +1,8 @@
+import { Badge } from '@/app/_components/ui/badge';
 import { Button } from '@/app/_components/ui/button';
 import { Card, CardContent } from '@/app/_components/ui/card';
 import { Barbershop } from '@prisma/client';
+import { FaStar } from "react-icons/fa";
 import Image from "next/image";
 import React from 'react';
 
@@ -12,7 +14,16 @@ const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
   return (
       <Card className='min-w-[167px] max-w-[167px] rounded-2xl border-primary-purple'>
       <CardContent className='p-0 px-1 text-white'>
-       <div className=" w-full h-[159px] relative">
+        <div className=" w-full h-[159px] relative">
+
+      <div className="absolute top-2 left-2 z-50 rounded-2xl bg-dark-purple text-primary-purple">
+        <Badge  className="flex gap-1 items-center top-3 left-3 ">
+              <FaStar className='bg-dark-purple' size={12} />
+              <span className='text-xs font-bold'>5,0</span>
+            
+          </Badge>
+          </div>
+          
          <Image src={barbershop.imageUrl} height={0} alt={barbershop.name}
         fill style={{objectFit:"cover",}} className="rounded-2xl" />
        </div>
