e0584697bd4a9c1acb8bff37836b3f69518ee4a3
Graciliana Kascher <eagraciliana@gmail.com>
2024-07-29 11:26:30 -0300

    [feat] add footer page

---
+++ src/app/_components/footer.tsx
@@ -0,0 +1,10 @@
+
+import * as React from 'react';
+
+export const Footer = () => {
+  return (
+    <div className='w-full py-6 px-5 className="pt-14 bg-[#1a1b1f]"'>
+      <p className=' text-gray-02 font-bold text-xl'>© 2023 Copyright FSW Barber</p>
+    </div>
+  );
+};
