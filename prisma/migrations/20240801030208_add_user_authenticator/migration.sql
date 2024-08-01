4f1ca2a24b3f3e3ba25504517625fdad0aede5fb
Graciliana Kascher <eagraciliana@gmail.com>
2024-08-01 17:28:08 -0300

    [remove] removed provider and section

---
+++ prisma/migrations/20240801030208_add_user_authenticator/migration.sql
@@ -0,0 +1,26 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `date` on the `booking` table. The data in that column could be lost. The data in that column will be cast from `Timestamp(0)` to `Timestamp`.
+
+*/
+-- AlterTable
+ALTER TABLE `booking` MODIFY `date` TIMESTAMP NOT NULL;
+
+-- CreateTable
+CREATE TABLE `Authenticator` (
+    `credentialID` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `providerAccountId` VARCHAR(191) NOT NULL,
+    `credentialPublicKey` VARCHAR(191) NOT NULL,
+    `counter` INTEGER NOT NULL,
+    `credentialDeviceType` VARCHAR(191) NOT NULL,
+    `credentialBackedUp` BOOLEAN NOT NULL,
+    `transports` VARCHAR(191) NULL,
+
+    UNIQUE INDEX `Authenticator_credentialID_key`(`credentialID`),
+    PRIMARY KEY (`userId`, `credentialID`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Authenticator` ADD CONSTRAINT `Authenticator_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
